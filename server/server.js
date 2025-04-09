2a18a51ffbaf5a26e83bd4e8329890b75f15d046
supakit <12evolte@gmail.com>
2025-04-09 22:27:38 +0700

    first draft of basic fucntion

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const http = require('http'); //reccommended for socket sol
 const {Server}= require('socket.io')
 const server = http.createServer(app)
 const cors= require('cors')
+const User = require('./models/User');
+const GroupChat = require('./models/GroupChat')
 
 dotenv.config({path:'./config/config.env'})
 connectDB(); //use db
@@ -27,15 +29,91 @@ const io= new Server(server,{
     },
 });
 
-io.on("connection",(socket)=>{
-    console.log(`User Connected: ${socket.id}`)
-    socket.on('join_room',(data)=>{
-        socket.join(data);
+//try to get token
+io.use((socket, next) =>{
+    // const token = socket.handshake.auth.token;
+    // const payload = j
+})
+io.on("connection",async (socket)=>{
+    console.log(`User Connected: ${socket.userId}`)//change to socket userId later
+
+    //join to everyroom this user is a member
+    const userId = socket.userId;
+    const userRooms = await User.findById(userId);
+    userRooms.room.array.forEach(room => {
+        socket.join(room._id.toString());
+    });
+
+    //create new room from type of room, and room members
+    socket.on('create_room', async (data)=>{
+        try{
+            const newroom = await GroupChat.create({
+                member: data.userIds,
+                type: data.type,
+                //roomid?
+            });
+            
+            //add to user's room
+            await User.updateOne({
+                _id: userId,
+            }, {$addToSet: {room: newroom._id}});
+
+            socket.join(newroom._id.toString());
+        }catch(err){
+            console.log(err);
+        }
+        
     })
 
-    socket.on("create-something",(data)=>{
-        socket.to(data.room).emit("receive_message",data)
-        console.log("check",data);
+    //join created group
+    socket.on('join_room',async (data)=>{
+        try{
+            await GroupChat.updateOne({
+                _id: data.roomId,               
+            },{ $addToSet: {member: data.userId}});
+
+            await User.updateOne({
+                _id: data.userId,
+            }, {$addToSet: {room: data.roomId}});
+            socket.join(data.roomId);
+        }catch(err){
+            console.log(err);
+        }
+    })
+
+    socket.on("create-something",async(data)=>{
+        try{
+            await GroupChat.updateOne({
+                _id: data.room,               
+            },{ $addToSet: {message: data}});
+            socket.to(data.room).emit("receive_message",data)
+            console.log("check",data);
+        }catch(err){
+            console.log(err);
+        }
+        
+    })
+
+    //all chat room?
+    socket.on("all_chat", (data)=>{
+        socket.broadcast(data);
+    })
+
+    //leave chat
+    socket.on('leave_room', async(data)=>{
+        await GroupChat.updateOne(
+            { _id: data.room},
+            {
+                $pull: {
+                    member: data.userId,
+                    message: {userId: data.userId}
+                }
+        })
+
+        await User.updateOne(
+            {_id: data.userId},
+            {$pull : {room: data.room}}
+        )
     })
 })
 
